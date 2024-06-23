1609a20d65b868830fdfcd6cdab6f681b3193180
Matheus <matheussmoura@outlook.com>
2024-06-22 22:53:08 -0300

    melhorado main.rs

--- lina/src/main.rs
+++ lina/src/main.rs
@@ -1,73 +1,44 @@
 use std::env;
-use std::fs::File;
-use std::io::{stdin, stdout, Read};
+use std::fs;
+use std::io::{stdin, stdout};
 
 use lina::compiler::compile;
-use lina::lexer::{lex, LexicalError};
-use lina::parser::{parse, SyntaxError};
-use lina::vm::{LinaVm, RuntimeError};
+use lina::lexer::lex;
+use lina::parser::parse;
+use lina::vm::LinaVm;
 
-pub struct LinaExec<'a> {
-    pub path: &'a str,
-    pub source: &'a str,
-}
-
-impl<'a> LinaExec<'a> {
-    fn lex_err(&mut self, err: LexicalError) {
-        eprintln!("Erro Léxico: {}", err.msg);
-        eprintln!("{}:{}:{}", self.path, err.row, err.col);
-    }
-
-    fn syn_err(&mut self, err: SyntaxError) {
-        eprintln!("Erro Sintático: {}", err.msg);
-        eprintln!("{}:{}:{}", self.path, err.pos.row, err.pos.col);
-    }
-
-    fn run_err(&mut self, err: RuntimeError) {
-        eprintln!("Erro: {err}");
-    }
-
-    pub fn run(&mut self) -> Result<(), ()> {
-        let tokens = lex(&self.source).map_err(|err| self.lex_err(err))?;
-        let program = parse(tokens).map_err(|err| self.syn_err(err))?;
-        let bytecode = compile(&program);
-
-        let mut vm = LinaVm::new(bytecode, stdin(), stdout());
-
-        vm.non_stop().map_err(|err| self.run_err(err))
-    }
-
-    pub fn decompile(&mut self) -> Result<(), ()> {
-        let tokens = lex(&self.source).map_err(|err| self.lex_err(err))?;
-        let program = parse(tokens).map_err(|err| self.syn_err(err))?;
-        let bytecode = compile(&program);
-
-        let mut vm = LinaVm::new(bytecode, stdin(), stdout());
-
-        vm.decompile().map_err(|err| self.run_err(err))
-    }
-}
-
-fn main() -> std::io::Result<()> {
+fn main() -> std::result::Result<(), ()> {
     let args = env::args().collect::<Vec<_>>();
-    let file_name = args.get(1).expect("nenhum arquivo especificado");
+    let file_path = args.get(1).ok_or(()).map_err(|_| {
+        eprintln!("Erro: arquivo não fornecido");
+    })?;
 
-    let mut file = File::open(file_name)?;
+    let code = fs::read_to_string(file_path).map_err(|err| {
+        eprintln!("Erro: não foi possivel ler o arquivo {file_path}: {err}");
+    })?;
 
-    let mut code = String::new();
-    file.read_to_string(&mut code)?;
+    let tokens = lex(&code).map_err(|err| {
+        eprintln!("Erro Léxico: {}", err.msg);
+        eprintln!("\t--> em {}:{}:{}", file_path, err.row, err.col);
+    })?;
 
-    let mut exec = LinaExec {
-        path: &file_name,
-        source: &code,
-    };
+    let program = parse(tokens).map_err(|err| {
+        eprintln!("Erro Sintático: {}", err.msg);
+        eprintln!("\t--> em {}:{}:{}", file_path, err.pos.row, err.pos.col);
+    })?;
+
+    let mut vm = LinaVm::new(compile(&program), stdin(), stdout());
 
     if let Some(arg) = args.get(2) {
         if arg == "-d" {
-            _ = exec.decompile();
+            vm.decompile().map_err(|err| {
+                eprintln!("Erro: {err}");
+            })?;
         }
     } else {
-        _ = exec.run();
+        vm.non_stop().map_err(|err| {
+            eprintln!("Erro: {err}");
+        })?;
     }
 
     Ok(())
