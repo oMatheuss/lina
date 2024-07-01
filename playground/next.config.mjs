b6d75b3d35d55961517ee93d0c9e2556ea0b1ea2
matheus <matheussmoura@outlook.com>
2024-07-01 10:14:04 -0300

    added BASE_PATH as env

--- playground/next.config.mjs
+++ playground/next.config.mjs
@@ -3,10 +3,11 @@ import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.BASE_PATH,
   webpack: (config, _ctx) => {
     config.plugins.push(new MonacoWebpackPlugin({
       filename: 'static/[name].worker.[contenthash].js',
-      publicPath: '/_next',
+      publicPath: (process.env.BASE_PATH ?? '') + '/_next',
       languages: [],
     }));
     return config;
